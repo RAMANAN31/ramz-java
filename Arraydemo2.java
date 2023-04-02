84526adccae7be00f4a779b3a2dbeac77a5d44f0
RAMANAN <112418260+RAMANAN31@users.noreply.github.com>
2023-04-02 19:39:11 +0530

    Add files via upload

---
+++ Arraydemo2.java
@@ -0,0 +1,14 @@
+public class Arraydemo2 {
+    /**
+     * @param args
+     */
+    public static void main(String[] args)
+	{
+		int marks[] = {23,53,26,43,54,34};
+		int i;
+		for(i=0;i<6;i++)
+		{
+		System.out.println(marks[i]);
+		}
+	}
+}
